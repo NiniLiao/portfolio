3103151b91433edc5146defb61c9ccc2c275503a
Nini Liao <u9513031@cmu.edu.tw>
2020-11-12 07:24:03 +0800

    modified js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 console.log('Its working')
 
-Let themeDots = document.getElementsByClassName('theme-dot')
+Let themeDots = document.getElementsByClassName('theme-dot');
 
 for (var i=0; themeDots.length > i; i++){
     themeDots[i].addEventListener('click', function(){
         Let mode = this.dataset.mode
         console.log('Option clicked:', mode)
+        setTheme(mode)
     })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+
+    }
 }
